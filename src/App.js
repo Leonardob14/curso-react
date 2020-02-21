b93dabaa13c92e5617bbd7db43721429195122bb
leonardo <leonardobuarque14@gmail.com>
2020-02-21 15:55:56 -0300

    comboBox

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello world 
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function ComponenteFUncional(){
+  return(
+    <h1>Teste</h1>
+  )
+}
+
+class App extends React.Component{
+
+  state = {
+    nome : ''
+  }
+
+  modificarNome = (event) =>{ 
+    this.setState({
+      nome : event.target.value
+    })
+  }
+
+  criaComboBox = () => {
+    const opcoes= ["Acreúna", "Goiânia", "Rio verde"]
+    const comboBoxOpcoes = opcoes.map( opcao => <option>{opcao}</option> )
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }  
+
+  render(){
+    return(
+        <>
+          <input type="text" placeholder="Digite seu nome" onChange={this.modificarNome} />
+          <h1>Olá {this.state.nome}</h1>
+          {this.criaComboBox()}
+        </>
+    )
+  }
 }
 
 export default App;
